1c67b3fd0bc14eb066e8016ce3c05eb67f9721fb
Denis Defreyne <denis.defreyne@stoneship.org>
2018-04-08 14:18:37 +0200

    Pretty-print ASTs

--- lib/clarke/ast.rb
+++ lib/clarke/ast.rb
@@ -4,106 +4,148 @@ module Clarke
   module AST
     # TODO: add context everywhere
 
+    module Printable
+      def inspect
+        to_s
+      end
+
+      def to_s
+        children_lines = ast_children.map(&:to_s).join("\n").lines
+
+        res = +''
+        res << '(' << ast_name
+        if ast_children.all? { |c| !c.respond_to?(:ast_children) || c.ast_children.empty? }
+          res << ' '
+          res << children_lines.first
+        else
+          res << "\n"
+          res << children_lines.map { |l| '  ' + l }.join('')
+        end
+        res << ')'
+        res
+      end
+    end
+
     FalseLiteral = Object.new
     TrueLiteral = Object.new
 
     Assignment = Struct.new(:variable_name, :expr) do
-      def inspect
-        to_s
+      include Printable
+
+      def ast_name
+        'Assignment'
       end
 
-      def to_s
-        "(Assignment #{variable_name} #{expr})"
+      def ast_children
+        [variable_name, expr]
       end
     end
 
     FunctionCall = Struct.new(:name, :arguments, :input, :old_pos, :new_pos) do
-      def inspect
-        to_s
+      include Printable
+
+      def ast_name
+        'FunctionCall'
       end
 
-      def to_s
-        "(FunctionCall #{name} #{arguments})"
+      def ast_children
+        [name, *arguments]
       end
     end
 
     If = Struct.new(:cond, :body_true, :body_false) do
-      def inspect
-        to_s
+      include Printable
+
+      def ast_name
+        'If'
       end
 
-      def to_s
-        "(If #{cond} #{body_true} #{body_false})"
+      def ast_children
+        [cond, body_true, body_false]
       end
     end
 
     IntegerLiteral = Struct.new(:value) do
-      def inspect
-        to_s
+      include Printable
+
+      def ast_name
+        'IntegerLiteral'
       end
 
-      def to_s
-        "(IntegerLiteral #{value})"
+      def ast_children
+        [value]
       end
     end
 
     LambdaDef = Struct.new(:argument_names, :body) do
-      def inspect
-        to_s
+      include Printable
+
+      def ast_name
+        'LambdaDef'
       end
 
-      def to_s
-        "(LambdaDef #{argument_names} #{body})"
+      def ast_children
+        [argument_names, body]
       end
     end
 
     Op = Struct.new(:name) do
-      def inspect
-        to_s
+      include Printable
+
+      def ast_name
+        'Op'
       end
 
-      def to_s
-        "(Op #{name})"
+      def ast_children
+        [name]
       end
     end
 
     OpSeq = Struct.new(:seq) do
-      def inspect
-        to_s
+      include Printable
+
+      def ast_name
+        'OpSeq'
       end
 
-      def to_s
-        "(OpSeq #{seq})"
+      def ast_children
+        [*seq]
       end
     end
 
     Scope = Struct.new(:exprs) do
-      def inspect
-        to_s
+      include Printable
+
+      def ast_name
+        'Scope'
       end
 
-      def to_s
-        "(Scope #{exprs})"
+      def ast_children
+        [*exprs]
       end
     end
 
     ScopedLet = Struct.new(:variable_name, :expr, :body) do
-      def inspect
-        to_s
+      include Printable
+
+      def ast_name
+        'ScopedLet'
       end
 
-      def to_s
-        "(ScopedLet #{variable_name} #{expr} #{body})"
+      def ast_children
+        [variable_name, expr, body]
       end
     end
 
     Var = Struct.new(:name) do
-      def inspect
-        to_s
+      include Printable
+
+      def ast_name
+        'Var'
       end
 
-      def to_s
-        "(Var #{name})"
+      def ast_children
+        [name]
       end
     end
   end
