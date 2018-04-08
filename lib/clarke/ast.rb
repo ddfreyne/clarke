050568c31e236d3c54b2306371595c7f0194bf84
Denis Defreyne <denis.defreyne@stoneship.org>
2018-04-08 09:10:00 +0200

    Simplify AST def

--- lib/clarke/ast.rb
+++ lib/clarke/ast.rb
@@ -2,41 +2,21 @@
 
 module Clarke
   module AST
-    IntegerLiteral = Struct.new(:value) do
-    end
-
-    TrueLiteral = Object.new
-    FalseLiteral = Object.new
-
     # TODO: add context everywhere
-    FunctionCall = Struct.new(:name, :arguments, :input, :old_pos, :new_pos) do
-    end
 
-    FunctionDef = Struct.new(:name, :argument_names, :body) do
-    end
+    FalseLiteral = Object.new
+    TrueLiteral = Object.new
 
-    LambdaDef = Struct.new(:argument_names, :body) do
-    end
-
-    Var = Struct.new(:name) do
-    end
-
-    Assignment = Struct.new(:variable_name, :expr) do
-    end
-
-    ScopedLet = Struct.new(:variable_name, :expr, :body) do
-    end
-
-    Scope = Struct.new(:exprs) do
-    end
-
-    If = Struct.new(:cond, :body_true, :body_false) do
-    end
-
-    Op = Struct.new(:name) do
-    end
-
-    OpSeq = Struct.new(:seq) do
-    end
+    Assignment = Struct.new(:variable_name, :expr)
+    FunctionCall = Struct.new(:name, :arguments, :input, :old_pos, :new_pos)
+    FunctionDef = Struct.new(:name, :argument_names, :body)
+    If = Struct.new(:cond, :body_true, :body_false)
+    IntegerLiteral = Struct.new(:value)
+    LambdaDef = Struct.new(:argument_names, :body)
+    Op = Struct.new(:name)
+    OpSeq = Struct.new(:seq)
+    Scope = Struct.new(:exprs)
+    ScopedLet = Struct.new(:variable_name, :expr, :body)
+    Var = Struct.new(:name)
   end
 end
