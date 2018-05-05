a604b379d7e829f829cc89e49f00060e19781e4d
Denis Defreyne <denis.defreyne@stoneship.org>
2018-05-05 11:46:28 +0200

    Sort ResolveSymbols methods

--- lib/clarke/passes/resolve_symbols.rb
+++ lib/clarke/passes/resolve_symbols.rb
@@ -3,16 +3,6 @@
 module Clarke
   module Passes
     class ResolveSymbols < Clarke::Visitor
-      def visit_ref(expr)
-        expr.name_sym = expr.scope.resolve(expr.name)
-        super
-      end
-
-      def visit_var_def(expr)
-        expr.var_name_sym = expr.scope.resolve(expr.var_name)
-        super
-      end
-
       def visit_assignment(expr)
         expr.var_name_sym = expr.scope.resolve(expr.var_name)
         super
@@ -35,6 +25,16 @@ module Clarke
         expr.params.each do |param|
           param.type_sym = expr.scope.resolve(param.type_name)
         end
+        super
+      end
+
+      def visit_ref(expr)
+        expr.name_sym = expr.scope.resolve(expr.name)
+        super
+      end
+
+      def visit_var_def(expr)
+        expr.var_name_sym = expr.scope.resolve(expr.var_name)
         super
       end
     end
