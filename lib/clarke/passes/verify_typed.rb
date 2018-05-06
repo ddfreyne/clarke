46244eb80978228166f5326825078fbb8d7c1aa9
Denis Defreyne <denis.defreyne@stoneship.org>
2018-05-06 09:05:31 +0200

    Verify no auto-type

--- lib/clarke/passes/verify_typed.rb
+++ lib/clarke/passes/verify_typed.rb
@@ -11,6 +11,8 @@ module Clarke
       def assert_typed(expr)
         if expr.type.nil?
           raise Clarke::Errors::GenericError.new("could not type #{expr.inspect}", expr: expr)
+        elsif expr.type.auto?
+          raise Clarke::Errors::GenericError.new("still auto-typed #{expr.inspect}", expr: expr)
         elsif !expr.type.is_a?(Clarke::Sym::Type)
           raise Clarke::Errors::GenericError.new("type is not a type #{expr.type.inspect} -- for #{expr.inspect}", expr: expr)
         end
