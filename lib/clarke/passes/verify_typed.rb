681c4e0802ba064b010e08f7d3e9e76260e4369c
Denis Defreyne <denis.defreyne@stoneship.org>
2018-05-06 10:00:35 +0200

    Verify presence of types even better

--- lib/clarke/passes/verify_typed.rb
+++ lib/clarke/passes/verify_typed.rb
@@ -8,13 +8,29 @@ module Clarke
         assert_typed(expr)
       end
 
-      def assert_typed(expr)
-        if expr.type.nil?
-          raise Clarke::Errors::GenericError.new("could not type #{expr.inspect}", expr: expr)
-        elsif expr.type.auto?
-          raise Clarke::Errors::GenericError.new("still auto-typed #{expr.inspect}", expr: expr)
-        elsif !expr.type.is_a?(Clarke::Sym::Type)
-          raise Clarke::Errors::GenericError.new("type is not a type #{expr.type.inspect} -- for #{expr.inspect}", expr: expr)
+      def visit_fun_def(expr)
+        expr.params.each do |param|
+          assert_typed(param)
+        end
+
+        super
+      end
+
+      def visit_lambda_def(expr)
+        expr.params.each do |param|
+          assert_typed(param)
+        end
+
+        super
+      end
+
+      def assert_typed(thing, expr: thing)
+        if thing.type.nil?
+          raise Clarke::Errors::GenericError.new("could not type #{thing.inspect}", expr: expr)
+        elsif thing.type.auto?
+          raise Clarke::Errors::GenericError.new("still auto-typed #{thing.inspect}", expr: expr)
+        elsif !thing.type.is_a?(Clarke::Sym::Type)
+          raise Clarke::Errors::GenericError.new("type is not a type #{thing.type.inspect}", expr: expr)
         end
       end
     end
